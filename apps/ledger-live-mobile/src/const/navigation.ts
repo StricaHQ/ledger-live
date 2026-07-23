d91ddec021b6b40016fcb4867e9c24f343ce969e
Naganath Bhat <162084254+naganathbhat@users.noreply.github.com>
2026-07-23 16:33:43 +0530

    feat: cardano drep for mobile

--- apps/ledger-live-mobile/src/const/navigation.ts
+++ apps/ledger-live-mobile/src/const/navigation.ts
@@ -499,6 +499,13 @@ export enum ScreenName {
   CardanoUndelegationConnectDevice = "CardanoUndelegationConnectDevice",
   CardanoUndelegationValidationError = "CardanoUndelegationValidationError",
   CardanoUndelegationValidationSuccess = "CardanoUndelegationValidationSuccess",
+  CardanoVoteDelegationStarted = "CardanoVoteDelegationStarted",
+  CardanoVoteDelegationSummary = "CardanoVoteDelegationSummary",
+  CardanoVoteDelegationSelectDRep = "CardanoVoteDelegationSelectDRep",
+  CardanoVoteDelegationSelectDevice = "CardanoVoteDelegationSelectDevice",
+  CardanoVoteDelegationConnectDevice = "CardanoVoteDelegationConnectDevice",
+  CardanoVoteDelegationValidationError = "CardanoVoteDelegationValidationError",
+  CardanoVoteDelegationValidationSuccess = "CardanoVoteDelegationValidationSuccess",
 
   // stacks
   StacksEditMemo = "StacksEditMemo",
@@ -745,6 +752,7 @@ export enum NavigatorName {
   // Cardano
   CardanoDelegationFlow = "CardanoDelegationFlow",
   CardanoUndelegationFlow = "CardanoUndelegationFlow",
+  CardanoVoteDelegationFlow = "CardanoVoteDelegationFlow",
   // NEAR
   NearStakingFlow = "NearStakingFlow",
   NearUnstakingFlow = "NearUnstakingFlow",
